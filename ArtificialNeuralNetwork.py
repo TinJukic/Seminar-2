079f7d58633263bf229b1e9f78395cc2dc8beb80
TinJukic <101596340+TinJukic@users.noreply.github.com>
2023-03-04 17:06:11 +0100

    Added basic classes

---
+++ ArtificialNeuralNetwork.py
@@ -0,0 +1,17 @@
+# Tin Jukić, Seminar 2
+# Artificial Neural Network
+
+class ArtificialNeuralNetwork:
+    """
+    Artificial Neural Network for solving Vehicle Routing Problem (VRP).\n
+    Author: Tin Jukić
+    """
+
+    def __init__(self, num_of_weights: int, num_of_layers: int = 1):
+        """
+        Constructor for creating an ANN.
+        :param num_of_weights: number of weights which the ANN has
+        :param num_of_layers: number of hidden layers which the ANN has
+        """
+        self._num_of_weights = num_of_weights + 1  # +w0!
+        self._num_of_layers = num_of_layers + 1  # +last layer!
---
+++ NNNeuron.py
@@ -0,0 +1,16 @@
+# Tin Jukić, Seminar 2
+# Artificial Neural Network Neuron
+
+class NNNeuron:
+    """
+    Neuron of the Artificial Neural Network.\n
+    Author: Tin Jukić
+    """
+
+    def __init__(self, num_of_weights: int):
+        """
+        Constructor for creating a neuron of the ANN.
+        :param num_of_weights: number of weights that the neuron has
+        """
+        self._num_of_weights = num_of_weights
+        self._weights = []
